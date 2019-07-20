b74b72fd5479ec5c6708c35cb3f9fced64d9539e
Julian Luís <julian.lfs@gmail.com>
2019-07-19 23:53:03 -0300

    added eureka client do service discovery

--- src/main/java/br/com/julianlfs/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/br/com/julianlfs/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package br.com.julianlfs.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("br.com.julianlfs.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
