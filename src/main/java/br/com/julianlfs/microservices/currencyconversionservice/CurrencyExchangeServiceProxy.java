1d52e91f4248859ad35ee4a195626acffa161bbd
Julian Luís <julian.lfs@gmail.com>
2019-07-19 21:05:23 -0300

    added ribbon pra load balance

--- src/main/java/br/com/julianlfs/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/br/com/julianlfs/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,16 @@
 package br.com.julianlfs.microservices.currencyconversionservice;
 
 import br.com.julianlfs.microservices.currencyconversionservice.domain.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//não precisa mais especificar a url pq nao vamos se comunicar com
+// um serviço rodando em porta especifica, o ribbom vai fazer load balance
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
