d4f1da5c36a6960022622e0199d3277d62af7987
Julian Luís <julian.lfs@gmail.com>
2019-07-21 23:36:44 -0300

    redirecionamento pro zuul

--- src/main/java/br/com/julianlfs/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/br/com/julianlfs/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 //não precisa mais especificar a url pq nao vamos se comunicar com
 // um serviço rodando em porta especifica, o ribbom vai fazer load balance
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retriveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
